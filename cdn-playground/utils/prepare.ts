ea3a4fd00b3367abe5fa73ca93fcfc7478a01e05
xietian2 <xietian2@myhexin.com>
2023-08-17 15:31:08 +0800

    build(cdn-prepare): only copy one css file

--- cdn-playground/utils/prepare.ts
+++ cdn-playground/utils/prepare.ts
@@ -3,17 +3,26 @@ import {
   buildLog,
   CDN_CONTAINER_DIST_PATH,
   getCdnFileDistPath,
+  getComponentLibOutputDir,
   ROOT_DIR,
 } from "../../utils";
 import { copyFile, removeSync } from "fs-extra";
 import { mkdirSync } from "fs-extra";
-import { extname } from "node:path";
+import { extname, resolve } from "node:path";
+
+/** 拷贝style文件，拷贝其中一个即可 */
+async function copyStyleFile(){
+  const mainCssName = 'style.css';
+  const sourceFile = resolve(getComponentLibOutputDir('v2'), mainCssName);
+  const targetFilePath = getCdnFileDistPath(mainCssName);
+  await copyFile(sourceFile, targetFilePath)
+}
 
 export async function prepareCdnDevEnv() {
   try {
     buildLog.start("prepare cdn dev env");
     buildLog.info("copy latest iife files to cdn-playground");
-    const sourceFiles = await fg("dist/**/(*.iife.js|style.css)", {
+    const sourceFiles = await fg("dist/**/*.iife.js", {
       onlyFiles: true,
       cwd: ROOT_DIR,
       absolute: true,
@@ -21,6 +30,7 @@ export async function prepareCdnDevEnv() {
     const taskList: Promise<void>[] = [];
     removeSync(CDN_CONTAINER_DIST_PATH);
     mkdirSync(CDN_CONTAINER_DIST_PATH);
+    // copy iife
     sourceFiles.forEach((filePath) => {
       const ext = extname(filePath);
       const filePathList = filePath.split("/");
@@ -28,8 +38,10 @@ export async function prepareCdnDevEnv() {
       const targetFilePath = getCdnFileDistPath(cdnFileName);
       taskList.push(copyFile(filePath, targetFilePath));
     });
+    // copy style
+    taskList.push(copyStyleFile());
     await Promise.all(taskList);
-    buildLog.success("iife files copied");
+    buildLog.success("iife and style files copied");
   } catch (e) {
     buildLog.error(e);
   }
