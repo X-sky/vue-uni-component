a6bfe8b0721878a8a5eba0e55fc37f62f70ba751
xietian2 <xietian2@myhexin.com>
2024-04-22 17:36:12 +0800

    test(component): redesign UniTemplate test

--- packages/components/UniTemplate/__test__/index.test.ts
+++ packages/components/UniTemplate/__test__/index.test.ts
@@ -1,12 +1,19 @@
 import { describe, expect, test } from "vitest";
 import { mount } from "@vue/test-utils";
 import UniTemplate from "../src/UniTemplate.vue";
+import { DEFAULT_MSG } from "../src/constants";
 
 describe("test UniTemplate", () => {
   const incBtnSelector = "[data-test=increase]";
   const decBtnSelector = "[data-test=decrease]";
   const numContentSelector = "[data-test=num]";
   test("test default", async () => {
+    const wrapper = mount(UniTemplate);
+    const title = wrapper.find(".uu-template__header");
+    // default show hello
+    expect(title.text()).toContain(DEFAULT_MSG);
+  });
+  test("test props", async () => {
     const testMsg = "test message";
     const wrapper = mount(UniTemplate, {
       // use propsData instead of props for backwards compatibility
@@ -15,11 +22,13 @@ describe("test UniTemplate", () => {
       },
     });
     const title = wrapper.find(".uu-template__header");
+    expect(title.text()).toContain(testMsg);
+  });
+  test("test click", async () => {
+    const wrapper = mount(UniTemplate);
     const incBtn = wrapper.find(incBtnSelector);
     const decBtn = wrapper.find(decBtnSelector);
     const num = wrapper.find(numContentSelector);
-    // default show hello
-    expect(title.text()).toContain(testMsg);
     expect(parseInt(num.text())).toBe(0);
     await incBtn.trigger("click");
     expect(parseInt(num.text())).toBe(1);
